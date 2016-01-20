b8574fe6b3566c25a35b95b4a8ac2f831c9686f9
Lukasz Haratym <lharatym@quantcast.com>
2016-01-20 18:12:00 +0000

    Tested nested dirs crawling.

--- test/test_tango.py
+++ test/test_tango.py
@@ -6,7 +6,15 @@ class TestCrawler:
     def test_given_empty_directory_return_empty_file_map(self):
         assert Crawler().crawl(path='resorces/dir1') == {}
 
-    def test_given_directory_with_files_return_file_paths_to_hashes(self):
-        discovered_files = Crawler().crawl(path='resources/dir2').keys()
+    def test_given_directory_with_one_file_return_file_paths_to_hashes(self):
+        assert Crawler().crawl(path='resorces/dir1') == {}
 
-        assert {'resources/dir2/one', 'resources/dir2/two'} == discovered_files
+    def test_given_directory_with_files_return_file_paths_to_hashes(self):
+        discovered_files = Crawler().crawl(path='resources/dir3').keys()
+
+        assert {'resources/dir3/two', 'resources/dir3/three'} == set(discovered_files)
+
+    def test_given_nested_directory_return_file_paths_to_hashes(self):
+        discovered_files = Crawler().crawl(path='resources/dir4').keys()
+
+        assert {'resources/dir4/four', 'resources/dir4/dir7/five'} == set(discovered_files)
