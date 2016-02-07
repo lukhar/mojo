ce6ba88ef8cf57fbd27976634a0f1a71a9e4a756
lukhar <lukasz.har@gmail.com>
2016-02-07 20:11:46 +0000

    Quick and dirty implementation, only crude support for py.test provided.

--- tango.py
+++ tango.py
@@ -1,12 +1,35 @@
-import os
+from __future__ import print_function
+import sys
+import time
+import subprocess
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 
-class Crawler:
+class FileDaemon(FileSystemEventHandler):
 
-    def crawl(self, path):
-        files = {}
-        for dirpath, _, filenames in os.walk(path):
-            for filename in filenames:
-                files[os.path.join(dirpath, filename)] = 1
+    def __init__(self, watched_dir, runner):
+        self.watched_dir = watched_dir
+        self.runner = runner
 
-        return files
+    def on_created(self, event):
+        print('hello ' + event.src_path)
+        subprocess.call(['py.test'])
+
+    def init(self):
+        observer = Observer()
+        observer.schedule(self, self.watched_dir)
+        observer.start()
+
+        print('started on: ' + self.watched_dir)
+        try:
+            while True:
+                time.sleep(2)
+        except KeyboardInterrupt:
+            observer.stop()
+        observer.join()
+
+if __name__ == '__main__':
+    watched_dir = sys.argv[1]
+
+    FileDaemon(watched_dir, runner=None).init()
